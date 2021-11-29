c67f8e0961a768ec7156a6f8262652230f8d971e
SergeyAndrienko <64705199+SergeyAndrienko@users.noreply.github.com>
2021-11-29 12:05:02 +0200

    Delete src directory

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import movieReducer from './movieSlice'
-
-export default configureStore({
-    reducer: {
-        popularMovies: movieReducer,
-    }
-});
